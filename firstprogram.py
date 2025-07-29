a593e4e1f4c84e3eaa02e689c946161c108b0c8a
Karma717 <karma17731134wangchuk@gmail.com>
2025-07-29 14:37:05 +0600

    New work

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,13 @@
 print("karma wangchuk")
 print("Hello world")
+a=2
+b=3
+sum=a+b
+print(sum)
+multi=a*b
+print(multi)
+print("Sum of a and b are:",sum)
+print("Product of a and b are :",multi)
+Name = input ("Enter your name :")
+Age = input ("Enter your Age :")
+print("Presenting you our HERO",Name,"Which is Living",Age,"years on EARTH!!! and eating EVERYTHING!!!")
