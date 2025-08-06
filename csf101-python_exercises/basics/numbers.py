9cd1b2c6ab517d932f48a469c78dc041f5c5eef9
Karma717 <karma17731134wangchuk@gmail.com>
2025-08-06 15:24:20 +0600

    exesice 1

---
+++ csf101-python_exercises/basics/numbers.py
@@ -0,0 +1,9 @@
+age = 20
+print(age)
+height=1.82
+print(height)
+age_in_days=age*365
+print(age_in_days)
+result=age/7
+print(result)
+
---
+++ csf101-python_exercises/basics/strings.py
@@ -0,0 +1,9 @@
+name="Karma wamgchuk"
+print(name)
+greetings="Hello,"+name+"!"
+print(greetings)
+greeting_f=f"Hello,{name}!"
+print(greeting_f)
+name_length=len(name)
+print(name_length)
+
